d2a7b7c948af2d3781251097be08ed853964b782
Adonis Dias <adonisdelima1@gmail.com>
2023-07-12 15:00:12 -0300

    Adicionando emoji | Adding emoji

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,3 +1,9 @@
+// useMemo é um hook do react que decora valores 
+import {useMemo} from 'react'
+
+// Importando nossos emojis 
+import emojis from '../../utils/emojis';
+
 import { 
     Container, 
     Profile, 
@@ -6,12 +12,21 @@ import {
 } from './styles'; 
 
 export default function MainHeader() {
+    const emoji = useMemo(() => {
+        //   Math.floor arredonda e o Math.random nos dá um número random 
+        // menor que 1 e então multiplicamos pelo tamanho do array 
+        const indice = Math.floor(Math.random() * emojis.length); 
+
+        //   Usamos o valor contido em indice para definir qual item do 
+        // array, ou seja, qual emoji foi sorteado 
+        return emojis[indice];
+    }, []);
     return(
         <Container>
             <h1>Toggle</h1>
 
             <Profile>
-                <Welcome>Olá, </Welcome>
+                <Welcome>Olá, {emoji}</Welcome>
                 <UserName>Adonis Dias</UserName>
             </Profile>
         </Container>
