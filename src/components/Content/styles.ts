cccf0c610816573bb60dd0d2a08e32e211669fa8
Adonis Dias <adonisdelima1@gmail.com>
2023-07-13 19:13:20 -0300

    feat: scrollbar to handle larger lists

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -13,4 +13,29 @@ export const Container = styled.div`
 
     // Especificando apenas um valor em padding afetamos os 4 lados 
     padding: 25px; 
+
+    // 100 = 100% da view port height (que é a altura da tela) 
+    //   Então fazemos 100vh - 70px para o content usar apenas a quantidade de 
+    // pixels que sobra depois que o cabeçalho pega 70px  
+    height: calc(100vh - 70px); 
+
+    //   O que não couber na área de conteúdo só deve ser exibido ao acionarmos 
+    // a barra de rolagem, para isso configuramos o campo overflow-y 
+    overflow-y: scroll; 
+    
+    
+    //   O uso do '&' antecedendo cada instrução é necessário para não sejam  
+    // ignoradas
+    &::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.colors.secondary};
+        border-radius: 10px;
+    } 
+
+    &::-webkit-scrollbar-track {
+        background-color: ${props => props.theme.colors.tertiary};
+    } 
 `
